a468719650c8303014c83eb15729ea6c8f16ad86
Leonid Tšigrinski <leonid.tsigrinski@skeletontech.com>
2023-09-21 12:01:22 +0300

    Kodu 1 + praks

--- Kodu1.java
+++ Kodu1.java
@@ -9,18 +9,96 @@
  *
  * Kasutatud allikad: 1) https://et.wikipedia.org/wiki/Mullsortimine
  *                    2) Aine Programmeerimine 2; Praktikum 9 (kiirmeetod); Autor: Athi Põder
+ *                    3) Kuidas konfigureerida printimine salvestakse failis:
+ *                       https://stackoverflow.com/questions/1994255/how-to-write-console-output-to-a-txt-file
+ *
+ * NB! Suurema sisendi puhul on vaja lisada -Xmx käsuread argument (VM Options).
+ * Muidu kiirmeetodis olevad mälu allokeerimised ei mahu
  *****************************************************************************/
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.Arrays;
 
 public class Kodu1 {
     public static void main(String[] args) {
-        int[] a = massiivJuhuslikult(10, 0, 10);
-        System.out.println(Arrays.toString(a));
-        a = kiirmeetod(a);
-        System.out.println(Arrays.toString(a));
-    }
+        long start, stop;
+        int[] massiiv;
+//        salvestaPrintimisedFailina("tulemused.txt");
 
+        /*
+        Esiteks, võrdlen ajakulu erijuhtudel. Neid on 3:
+        1) Väike sisend (0, 1, 2)
+        2) Parim juht (sorteeritud)
+        3) Halvim juht (kahenevalt sorteeritud)
+        Pärast, võrdlen ajakulu keskmisel juhtudel, kui sisend on juhuslik massiiv.
+        Antud koodis iga mõõtmise jaoks teen 10 sämplit, et keerukuse hindamisel arvestada keskmist.
+         */
+
+//        /* Erijuht 1. Väiksed suurused */
+//        System.out.println("[Erijuht 1. n kuulub {0, 1, 2}]");
+//        System.out.println("[Sisendi suurus (n)\tAeg (ns)]"); // Legend
+//        for (int n = 0; n <= 2; n++) {
+//            massiiv = massiivKasvavalt(n); // Väiksema sisendi puhul vahet ei ole, kas sorteeritud või mitte
+//
+//            System.out.println("<Mullimeetod>");
+//            for (int i = 0; i < 10; i++) {
+//                start = System.nanoTime();
+//                mullimeetod(massiiv);
+//                stop = System.nanoTime();
+//                System.out.printf("%d\t%d\n", n, (stop - start));
+//            }
+//
+//            System.out.println("<Kiirmeetod>");
+//            for (int i = 0; i < 10; i++) {
+//                start = System.nanoTime();
+//                kiirmeetod(massiiv);
+//                stop = System.nanoTime();
+//                System.out.printf("%d\t%d\n", n, (stop - start));
+//            }
+//
+//            System.out.println("<java.util.Arrays.sort>");
+//            for (int i = 0; i < 10; i++) {
+//                start = System.nanoTime();
+//                susteemimeetod(massiiv);
+//                stop = System.nanoTime();
+//                System.out.printf("%d\t%d\n", n, (stop - start));
+//            }
+//        }
+
+        /* Erijuht 2. Parim juht */
+        System.out.println("[Erijuht 2. Sorteeritud");
+        System.out.println("[Sisendi suurus (n)\tAeg (ms)]"); // Legend
+        for (int n = 0; n <= 10_000; n += 100) {
+            massiiv = massiivKasvavalt(n);
+
+            System.out.println("<Mullimeetod>");
+            for (int i = 0; i < 10; i++) {
+                start = System.currentTimeMillis();
+                mullimeetod(massiiv);
+                stop = System.currentTimeMillis();
+                System.out.printf("%d\t%d\n", n, (stop - start));
+            }
+
+            System.out.println("<Kiirmeetod>");
+            for (int i = 0; i < 10; i++) {
+                start = System.currentTimeMillis();
+                kiirmeetod(massiiv);
+                stop = System.currentTimeMillis();
+                System.out.printf("%d\t%d\n", n, (stop - start));
+            }
+
+            System.out.println("<java.util.Arrays.sort>");
+            for (int i = 0; i < 10; i++) {
+                start = System.currentTimeMillis();
+                susteemimeetod(massiiv);
+                stop = System.currentTimeMillis();
+                System.out.printf("%d\t%d\n", n, (stop - start));
+            }
+        }
+
+    }
 
 
     /**
@@ -29,15 +107,15 @@ public class Kodu1 {
      * Kui vaadeldav element suurem, kui järgmine, siis vahetame neid (ehk nihutame paremale) nii kaua, kui saab.
      * Järgmise elemndi puhul sammude arv on ühe (i) võrra väiksem, sest eelmine vaadeldav element on juba lõppus.
      *
-     * @param m - massiiv, mida sorteeritakse
-     * @return sorteeritud massiiv
+     * @param massiiv - antud, ei muuta
+     * @return uus sorteeritud massiiv
      */
-    public static int[] mullimeetod(int[] m) {
-        if (m.length <= 1)
-            return m;
+    public static int[] mullimeetod(int[] massiiv) {
+        if (massiiv.length <= 1)
+            return massiiv;
 
-        int suurus = m.length;
-        int[] uus = kopeeriAlgus(m, suurus);
+        int suurus = massiiv.length;
+        int[] uus = kopeeriAlgus(massiiv, suurus);
 
         for (int i = 0; i < suurus - 1; i++)
             for (int j = 0; j < suurus - 1 - i; j++)
@@ -56,28 +134,28 @@ public class Kodu1 {
      * Teeme sama asja rekursiivlest, kasutades vasak ja parempoolne massiiv. Ühildame kõik osasid vasakult paremale,
      * sest neid on juba sorteeritud. See on binaarne rekursioon.
      *
-     * @param m
-     * @return sorteeritud massiiv
+     * @param massiiv antud, ei muuta
+     * @return uus sorteeritud massiiv
      */
-    public static int[] kiirmeetod(int[] m) {
+    public static int[] kiirmeetod(int[] massiiv) {
         // Rekursiooni baas
-        if (m.length <= 1)
-            return m;
+        if (massiiv.length <= 1)
+            return massiiv;
 
-        int x = m[0]; // Mille järgi me jagame pooleks, "pivot"
-        int[] vasak = new int[m.length];
-        int[] kesk = new int[m.length];
-        int[] parem = new int[m.length];
+        int x = massiiv[0]; // Mille järgi me jagame pooleks, "pivot"
+        int[] vasak = new int[massiiv.length];
+        int[] kesk = new int[massiiv.length];
+        int[] parem = new int[massiiv.length];
         int suurusVasak = 0, suurusKesk = 0, suurusParem = 0;
 
         // Jaotan kolmeks alam massiiviks. Täidan
-        for (int i = 0; i < m.length; i++) {
-            if (m[i] < x)
-                vasak[suurusVasak++] = m[i];
-            else if (m[i] == x)
-                kesk[suurusKesk++] = m[i];
+        for (int elem : massiiv) {
+            if (elem < x)
+                vasak[suurusVasak++] = elem;
+            else if (elem == x)
+                kesk[suurusKesk++] = elem;
             else
-                parem[suurusParem++] = m[i];
+                parem[suurusParem++] = elem;
         }
 
         // Juba täidetud. Nüüd muudan suurus
@@ -90,7 +168,7 @@ public class Kodu1 {
         parem = kiirmeetod(parem);
 
         // Osade kokkupanek
-        int[] uus = new int[m.length];
+        int[] uus = new int[massiiv.length];
         int i = 0;
         for (int k : vasak)
             uus[i++] = k;
@@ -99,6 +177,20 @@ public class Kodu1 {
         for (int k : parem)
             uus[i++] = k;
 
+        return uus;
+    }
+
+    /**
+     * <java.util.Arrays.sort> meetodi modifikatsioon, et ta ei sorteeriks etteantud, vaid tagastab uue.
+     * Põhjuseks on see, et kuna ma tahan teha mitu sämplid, on vaja, et originaal massiiv jääb samaks.
+     * Muidu üks mõõt on õge ja plejaanud on parima juhu.
+     *
+     * @param massiiv antud, ei muuta
+     * @return uus sorteeritud massiiv
+     */
+    public static int[] susteemimeetod(int[] massiiv) {
+        int[] uus = kopeeriAlgus(massiiv, massiiv.length);
+        Arrays.sort(uus);
         return uus;
     }
 
@@ -143,5 +235,16 @@ public class Kodu1 {
             m[i] = (int) ((Math.random() * (max - min + 1)) + min); // + 1 selleks, et ülemine piir oleks kaasarvatud
 
         return m;
+    }
+
+    /* Abi. Konfigureerib voog, et kõik printimised salvestakse failina */
+    public static void salvestaPrintimisedFailina(String failinimi) {
+        PrintStream out;
+        try {
+            out = new PrintStream(new FileOutputStream(failinimi));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        System.setOut(out);
     }
 }
