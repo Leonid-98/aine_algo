76f2e21ecc8863ffe091edc43aebdda9dba5fecf
Leonid Tšigrinski <leonid.tsigrinski@skeletontech.com>
2023-10-06 19:39:14 +0300

    Kodu 2b

--- Kodu2B.java
+++ Kodu2B.java
@@ -1,10 +1,31 @@
+import java.util.Arrays;
+import java.util.Stack;
+
 public class Kodu2B {
     public static void main(String[] args) {
-
+        long[] a = {10, 11};
+        int p = 15;
+        tükeldused(a, p);
     }
 
-    public static int tükeldused(long[] a, long p) {
-        return 0;
+    public static void tükeldused(long[] massiiv, long pikkus) {
+        Arrays.sort(massiiv);
+        long minimum = massiiv[massiiv.length - 1];
+        tükeldused(massiiv, pikkus, minimum, 0, new Stack<>(), 0);
+    }
+
+    public static void tükeldused(long[] massiiv, long pikkus, long minimum, int start, Stack<Long> magasin, long summa) {
+        if (summa > pikkus) {
+            System.out.println(magasin);
+            return;
+        }
+
+
+        for (int i = start; i < massiiv.length; i++) {
+                magasin.push(massiiv[i]);
+                tükeldused(massiiv, pikkus, minimum, i, magasin, summa + massiiv[i]);
+                magasin.pop();
+        }
     }
 
 
